9f945bce96a8e46c2f6bbd7dd4930cb95229402f
Mystery032 <150342695+Mystery032@users.noreply.github.com>
2023-11-09 12:19:05 +0000

    Falling Rock Fire BETA

--- Assets/Scripts/FallingRocks.cs
+++ Assets/Scripts/FallingRocks.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingRocks : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float FallSpeed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //rocks falling down
+        transform.Translate(Vector3.down * FallSpeed * Time.deltaTime);
+
+        // Checking the rock is on the y axis 
+        if (transform.position.y < -5f)
+        {
+            ResetRockPosition();
+        }
+        void ResetRockPosition()
+        {
+            // Rock will be above but on screen 
+            float randomX = Random.Range(-5f, 5f);
+            float maxY = 10f;
+            transform.position = new Vector3(randomX, maxY, 0f);
+        }
     }
-}
+ }
