5a04e100bacafe7090b68c74096a2069020452b5
SneakyPotato9 <150342927+SneakyPotato9@users.noreply.github.com>
2023-11-09 14:35:24 +0000

    Test Scores

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public int scoreValue = 1;
+    private float ScoreHandler;
+    public float score;
 
     public void Collect()
     {
-     //   ScoreManager.Instance.AddScore(scoreValue);
+        ScoreManager.Instance.AddScore(scoreValue);
 
         Destroy(gameObject);
     }
+
 
 }
