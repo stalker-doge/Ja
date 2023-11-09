463150810d76f25e141fcd430a4b60e1590c3873
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2023-11-09 12:25:43 +0000

    Added Multiplayer

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -13,6 +13,13 @@ public class PlayerCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveSpeed = 10;
+        //Define the speed at which the object moves.
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //Get the value of the Horizontal input axis.
+
+        transform.Translate(new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime);
+        //Move the object to XYZ coordinates defined as horizontalInput, 0,0
     }
 }
